b8666056e7b5611d03bec5a1c2456cb2c9dd56ec
KokHwaLim <kh60lim@gmail.com>
2017-01-26 10:16:00 +0800

    final version 1

--- javascript/index.js
+++ javascript/index.js
@@ -1,56 +1,17 @@
-$(function () {
-
-    // on submitting the form
-    $('form').submit(function (event) {
-        // prevent the default action of reloading the page
-        event.preventDefault();
-
-        var sendData = {};
-        $(event.target.nodeName + ' :input').each(function(){
-            sendData[this.name] = $(this).val();
-        });
-
-        var posting = $.ajax({
-            type: 'POST',
-            url: $(event.target.nodeName).prop('action'),
-            data: sendData
-        });
-
-        posting.done(function (response) {
-            console.log(response);
-            
-            if($('#alert-id').length > 0){
-                $('#alert-id').prop('hidden', false);
-            } else {
-                $('form').append('<p>Thank you</p>')
-            }
-            
-            $('form :input').each(function(){
-                $(this).val('');
-            })
-        });
-        posting.fail(function (response) {
-            console.log(response);
-        });
-    });
-
-// RESPONSE ALERT WINDOW-------------------------------------------------------------------------------
-/* include the following HTML to use:
-<div class="form-group">
-    <button type="submit" class="btn btn-default my-btn form-control" id="submit-id">submit</button>                   
-    <div class="alert alert-danger alert-dismissible fade in" hidden id="alert-id">
-        <button type="button" class="close" id="close-id"><span>&times;</span></button>
-        Thank you! I will get in touch.
-    </div>
-</div>
-*/
-
-    // on clicking the X button
-    $('#close-id').click(function(){
-        // hide the alert panel by adding the hidden property
-        $('#alert-id').prop('hidden', true);
-        // optionally reload the webpage
-        location.reload();
-    });
-
-});
+function ImagineCalc(){
+var mynumber;
+mynumber = Math.floor((Math.random() * 6) + 1);
+if (mynumber == 1){
+		document.getElementById('imagine-text').innerHTML = "La imaginación también puede expresarse a través de historias como cuentos de hadas o fantasías. Los niños usan a menudo tales narraciones y fingen el juego para ejercitar su imaginación.Cuando los niños desarrollan la fantasía, juegan a dos niveles: en primer lugar, utilizan el juego de roles para representar lo que han desarrollado con su imaginación y en el segundo nivel juegan de nuevo con su situación de fantasía actuando como si lo que han desarrollado es Una realidad real.";}
+	else if (mynumber == 2){
+		document.getElementById('imagine-text').innerHTML = "La imaginación es una partición experimental de la mente utilizada para desarrollar teorías e ideas basadas en funciones. Tomando objetos de las percepciones reales, la imaginación utiliza complejas funciones IF [cita requerida] para desarrollar ideas nuevas o revisadas. Esta parte de la mente es vital para desarrollar maneras mejores y más fáciles de lograr tareas viejas y nuevas. En sociología, la imaginación se utiliza para apartarse de la realidad y tener una comprensión de las.";}
+	else if (mynumber == 3){
+		document.getElementById('imagine-text').innerHTML = "Esto conduce al desarrollo de teorías a través de preguntas que normalmente no se harían. Estas ideas experimentales se pueden conducir con seguridad dentro de un mundo virtual y entonces, si la idea es probable y la función es verdadera, la idea se puede actualizar en la realidad. La imaginación es la clave para el nuevo desarrollo de la mente y puede ser compartida con otros, progresando colectivamente. Interacciones sociales derivadas de una perspectiva fuera de la sociedad misma.";}
+	else if (mynumber == 4){
+		document.getElementById('imagine-text').innerHTML = "La imaginación es diferente de la creencia porque el sujeto entiende que lo que es personalmente inventado por la mente no necesariamente afecta el curso de acción tomado en el mundo aparentemente compartido, mientras que las creencias son parte de lo que uno sostiene como verdades sobre los mundos compartido y personal. El juego de la imaginación, aparte de las obvias limitaciones (por ejemplo, de evitar la autocontradición explícita), está condicionado sólo por la tendencia general.";}
+	else if (mynumber == 5){
+		document.getElementById('imagine-text').innerHTML = "La creencia, por otra parte, se relaciona inmediatamente con la actividad práctica: es perfectamente posible imaginarse a sí mismo millonario, pero a menos que uno crea que uno no actúa como tal. La creencia se esfuerza por ajustarse a las condiciones experimentadas del sujeto oa la fe en la posibilidad de esas condiciones; Mientras que la imaginación como tal es específicamente libre. La línea divisoria entre imaginación y creencia varía ampliamente en las diferentes etapas del desarrollo.";}
+	else if (mynumber == 6){
+		document.getElementById('imagine-text').innerHTML = "El mundo como experimentado es una interpretación de los datos que llegan de los sentidos; Como tal, se percibe como real en contraste con la mayoría de los pensamientos e imaginaciones. Se dice que los usuarios de drogas alucinógenas tienen una mayor imaginación. Esta diferencia es sólo una de grado y puede ser alterada por varias causas históricas, a saber, cambios en la química del cerebro, la hipnosis u otros estados alterados de la conciencia, la meditación tecnológico.";}
+		else {document.getElementById('imagine-text').innerHTML = "El mundo como experimentado es una interpretación de los datos que llegan de los sentidos; Como tal, se percibe como real en contraste con la mayoría de los pensamientos e imaginaciones. Se dice que los usuarios de drogas alucinógenas tienen una mayor imaginación. Esta diferencia es sólo una de grado y puede ser alterada por varias causas históricas, a saber, cambios en la química del cerebro, la hipnosis u otros estados alterados de la conciencia, la meditación tecnológico.";}
+};
